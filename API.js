1b9509f00c937fe0e5335dfbcb23ef9ee94d6b91
marvin <marvin.lanz@besonet.ch>
2021-09-25 02:44:26 +0200

    Removed unnecessary file

--- API.js
+++
@@ -1,7 +0,0 @@
-import { LolApi, Constants } from 'twisted'
-
-const api = new LolApi()
-
-export async function summonerByNameExample () {
-  return await api.Summoner.getByName('Hide on bush', Constants.Regions.KOREA)
-}
